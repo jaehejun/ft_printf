11cfbe7309cc76bcaa536dba1b1eba87901d0c46
Jaehee Jung <wogml414@gmail.com>
2023-04-14 19:59:09 +0900

    printf_eval2ice

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jaehejun <jaehejun@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/11 20:43:43 by jaehejun          #+#    #+#              #
-#    Updated: 2023/04/13 13:27:53 by jaehejun         ###   ########.fr        #
+#    Updated: 2023/04/14 19:58:51 by jaehejun         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -36,3 +36,9 @@ fclean: clean
 re:
 	make fclean
 	make all
+
+
+
+
+	//libft 아카이브 사용하려면 ./libft로 가서 libft.a를 만든후에
+	//libft.a와 printf.c 들을 묶어서 libftprintf.a 로 만들어야 함
--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,51 @@
 /*   By: jaehejun <jaehejun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 20:48:51 by jaehejun          #+#    #+#             */
-/*   Updated: 2023/04/13 20:07:11 by jaehejun         ###   ########.fr       */
+/*   Updated: 2023/04/14 19:58:53 by jaehejun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_printf(const char *format, ...)
+int	check_format(char *format, )
 {
 	if (format == %c)
-
+		ft_putchar();
 	else if (format == %s)
-
+		ft_putstr();
 	else if (format == %p)
-
-	else if (format == %d)
-
-	else if (format == %i)
-
-	else if (format == %u)
-
-	else if (format == %x)
-
-	else if (format == %X)
-
-	else if (format == %%)
 		write()
+	else if (format == %d)
+		write()
+	else if (format == %i)
+		write()
+	else if (format == %u)
+		write()
+	else if (format == %x)
+		write()
+	else if (format == %X)
+		write()
+	else if (format == %%)
+		write(1, "%", 1);
 }
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+
+	va_start(ap, format)
+	va_arg(ap, )
+
+
+	va_end()
+}
+
+
+
+int	count;
+error check() // %ㄲㅏ지 밀고 %찾으면 그 뒤에 유효한 format이 있는지 검사
+va_start() // 고정된 char* 밀고 첫번째 가변인자의 시작주소를 가리킴
+%찾고 뒤의 포멧에 따라 
+해당 함수로 가서(ft_conversion()) malloc 후 출력, 반환할 count++;
+va_end()
+return (cnt);;
